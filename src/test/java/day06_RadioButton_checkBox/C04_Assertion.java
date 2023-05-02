362ecf1a40bc841e7f4c1f61a069471b843b67bf
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-05-02 16:17:38 +0300

    02.05.2023

--- src/test/java/day06_RadioButton_checkBox/C04_Assertion.java
+++ src/test/java/day06_RadioButton_checkBox/C04_Assertion.java
@@ -2,6 +2,7 @@ package day06_RadioButton_checkBox;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
@@ -36,8 +37,35 @@ public class C04_Assertion {
         /*
         Eğer test methodumuzda hiçbir test yoksa test çalıştıktan sonra
         hiçbir problemle karşılaşılmadığını raporlamak için"test passed" yazısı çıkar.
-        Eğer testlerii if ile yaparsak test failed olsa bile kodlar problemsiz çalıştığı için ekranın sol alt tarafında test passed yapar.
+        Eğer testleri if ile yaparsak test failed olsa bile kodlar problemsiz çalıştığı için ekranın sol alt tarafında test passed yapar.
          */
         driver.get("https://amazon.com");
+        /* url'nin https://facebook.com olduğunu test edin.
+        if (driver.getCurrentUrl().equals("https://facebook.com")){
+            System.out.println("url testi PASSED");
+        }else {
+            System.out.println("url testi FAILED");
+        }*/
+
+        String expectedUrl="https://facebook.com";
+        String actualUrl="https://amazon.com";
+        Assert.assertEquals("Url beklenenden farkli",expectedUrl,actualUrl);//istersek başa bir mesaj yazabiliriz/bu mesaj sadece failed olduğunda çıkar
+        //Assert Classı JUnit'den geliyor artık if-else bloklarına ihtiyacımız kalmadı.
+        /*
+        Assert ile yaptığımız testlerde assertion failed olursa Java
+        kodları çalışmayı durdurur ve Assert Classı bizi hata konusunda bilgilendirir.
+
+        org.junit.ComparisonFailure: Url beklenenden farklı :
+        Expected :https://facebook.com
+        Actual   :https://www.amazon.com/
+        <Click to see difference>
+         */
+        /*
+         Kod hiçbir assertion ile karşılaşmayıp doğru olarak çalıştığında istediğimiz test failed olsa da
+         test passed olur.
+          */
+
+
+
     }
 }
