b04da5cfa657855e483c5de95127d4de3728f1d0
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-05-02 14:39:20 +0300

    02.05.2023

--- src/test/java/day06_RadioButton_checkBox/C01_BeforeClass_AfterClass.java
+++ src/test/java/day06_RadioButton_checkBox/C01_BeforeClass_AfterClass.java
@@ -2,16 +2,14 @@ package day06_RadioButton_checkBox;
 
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
 /*
-BeforeClass  ve AfterClass methodları sadece static methodlar için çalışır.Class da sadece 1 kere çalışır.
+BeforeClass  ve AfterClass notasyonları sadece static methodlar için çalışır.Class da sadece 1 kere çalışır.
 Before ve After notasyonları her methoddan önce ve her methoddan sonra çalısır.
 Ama BeforeClass  ve AfterClass methodları sadece class'ın basında ve class'ın sonunda çalısır.
 **Before ve After da gidiyo test1 i yapıyo kapatıyo test2 yi yapıyo kapatıyo...**
@@ -19,18 +17,19 @@ Ama BeforeClass  ve AfterClass methodları sadece class'ın basında ve class'ı
 BeforeClass(precondition)->örneğin test methodlarımız çalışmadan driver oluşturup tüm methodlarda kullanabilirm.
 AfterClass->örneğin açtığımız sayfayı kapatmak veya elde ettiğimiz test sonuçlarını raporlamak gibi.
  */
+
 public class C01_BeforeClass_AfterClass {
-    WebDriver driver;
-    @Before
-    public void setUp(){
+    static WebDriver driver;
+    @BeforeClass
+    public static void setUp(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
     }
 
-    @After
-    public void tearDown(){
+    @AfterClass
+    public static void tearDown(){
         driver.close();
     }
 
