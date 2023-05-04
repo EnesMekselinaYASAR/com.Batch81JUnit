a3a4dc1ba6b1b76084c681396a2d36aef5d276c1
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-05-04 16:41:41 +0300

    04.05.2023

--- src/test/java/day07_Assertions/C06_DropDown.java
+++ src/test/java/day07_Assertions/C06_DropDown.java
@@ -4,10 +4,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.time.Duration;
+import java.util.List;
 
 public class C06_DropDown {
     /*
@@ -16,7 +20,7 @@ public class C06_DropDown {
     ıı.Index kullanarak Seçenek 1’i (Option 1) seçin ve yazdırın
     ııı.Value kullanarak Seçenek 2'yi (Option 2) seçin ve yazdırın
     ıv.Visible Text(Görünen metin) kullanarak Seçenek 1’i (Option 1) seçin ve yazdırın
-    v.Tüm dropdown değerleri(value) yazdırın
+    v.Tüm dropdown optionlari yazdırın
     vı.Dropdown'un boyutunu bulun, Dropdown'da 4 öğe varsa konsolda True , degilse
     False yazdırın.
     */
@@ -32,10 +36,35 @@ public class C06_DropDown {
 
     @After
     public void tearDown(){
-        driver.close();
+        //driver.close();
     }
 
     @Test
     public void test01(){
+        driver.get("https://the-internet.herokuapp.com/dropdown");
+        WebElement ddm=driver.findElement(By.xpath("//select[@id='dropdown']"));
+        Select select=new Select(ddm);
+        select.selectByIndex(1);
+        System.out.println(select.getFirstSelectedOption().getText());
+
+        select.selectByValue("2");
+        System.out.println(select.getFirstSelectedOption().getText());
+
+        select.selectByVisibleText("Option 1");
+        System.out.println(select.getFirstSelectedOption().getText());
+
+        List<WebElement> tumOpsiyonlar=select.getOptions();
+        for (WebElement each: tumOpsiyonlar
+             ) {
+            System.out.println(each.getText());
+        }
+
+        int dropDownBoyut=tumOpsiyonlar.size();
+        if (dropDownBoyut==4){
+            System.out.println("true");
+        }else {
+            System.out.println("false");
+        }
+
     }
 }
