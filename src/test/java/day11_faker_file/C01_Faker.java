16b9eb46b0ca4e9c9d480170cd63d7d26c0ee59b
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-05-09 10:17:39 +0300

    09.05.2023

--- src/test/java/day11_faker_file/C01_Faker.java
+++ src/test/java/day11_faker_file/C01_Faker.java
@@ -1,6 +1,7 @@
 package day11_faker_file;
 
 import Utilities.TestBase;
+import com.github.javafaker.Faker;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -14,6 +15,7 @@ public class C01_Faker extends TestBase {
         // 1- https://www.facebook.com adresine gidelim.
         driver.get("https://www.facebook.com");
         Actions actions = new Actions(driver);
+        Faker faker=new Faker();
 
 
         // 2- Yeni hesap olustur butonuna basalim.
@@ -22,32 +24,34 @@ public class C01_Faker extends TestBase {
 
         // 3- Ad, soyad, mail ve sifre kutularina deger yazalim ve kaydol tusuna basalim.
         WebElement name = driver.findElement(By.xpath("(//input[@type='text'])[2]"));
-        actions.click(name).sendKeys("muzaffer")
+        String fakeMail=faker.internet().emailAddress();
+        actions.click(name)
+                .sendKeys(faker.name().firstName())
                 .sendKeys(Keys.TAB)
-                .sendKeys("ugur").
-                sendKeys(Keys.TAB).
-                sendKeys("ornek@gmail.com")
-                .sendKeys(Keys.TAB).
-                sendKeys("ornek@gmail.com").
-                sendKeys(Keys.TAB)
-                .sendKeys("147856").
-                sendKeys(Keys.TAB).
-                sendKeys(Keys.TAB)
-                .sendKeys("5").
-                sendKeys(Keys.TAB).
-                sendKeys("Tem").
-                sendKeys(Keys.TAB).
-                sendKeys("1995")
+                .sendKeys(faker.name().lastName())
                 .sendKeys(Keys.TAB)
-                .sendKeys(Keys.TAB).
-                sendKeys(Keys.RIGHT).
-                sendKeys(Keys.TAB)
-                .sendKeys(Keys.TAB).
-                sendKeys(Keys.TAB).
-                sendKeys(Keys.TAB).
-                sendKeys(Keys.TAB).
-                sendKeys(Keys.ENTER).
-                perform();
+                .sendKeys(fakeMail)
+                .sendKeys(Keys.TAB)
+                .sendKeys(fakeMail)
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.internet().password())
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys("29")
+                .sendKeys(Keys.TAB)
+                .sendKeys("Haz")
+                .sendKeys(Keys.TAB)
+                .sendKeys("1995")
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.RIGHT)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.ENTER)
+                .perform();
 
         Thread.sleep(3000);
     }
