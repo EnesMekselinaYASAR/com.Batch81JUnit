5db2ccb150404b34a14c9022c75d71172434b284
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-05-10 16:28:07 +0300

    10.05.2023

--- src/test/java/day11_faker_file/C02_KeyboardActions.java
+++ src/test/java/day11_faker_file/C02_KeyboardActions.java
@@ -1,7 +1,12 @@
 package day11_faker_file;
 
 import Utilities.TestBase;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class C02_KeyboardActions extends TestBase {
         /*
@@ -11,8 +16,20 @@ public class C02_KeyboardActions extends TestBase {
         4.videoyu çalıştırdığınızı test edin
         */
     @Test
-    public void keyboardActionsTest(){
+    public void keyboardActionsTest() throws InterruptedException {
         driver.get("https://html.com/tags/iframe/");
+        Actions actions=new Actions(driver);
+        actions.sendKeys(Keys.PAGE_DOWN)
+                .sendKeys(Keys.PAGE_DOWN)
+                .perform();
+
+        WebElement iframe=driver.findElement(By.xpath("//iframe[@src='https://www.youtube.com/embed/owsfdh4gxyc']"));
+        driver.switchTo().frame(iframe);
+        driver.findElement(By.xpath("//button[@class='ytp-large-play-button ytp-button ytp-large-play-button-red-bg']")).click();
+        Thread.sleep(1000);
+
+        WebElement youTubeLinki=driver.findElement(By.xpath("//a[@class='ytp-youtube-button ytp-button yt-uix-sessionlink']"));
+        Assert.assertTrue(youTubeLinki.isDisplayed());
 
     }
 }
---
+++ src/test/java/day11_faker_file/C03_FileExist.java
@@ -0,0 +1,8 @@
+package day11_faker_file;
+
+import Utilities.TestBase;
+
+public class C03_FileExist {
+
+
+}
