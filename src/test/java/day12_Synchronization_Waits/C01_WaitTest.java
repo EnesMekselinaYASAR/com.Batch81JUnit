882d52d461222ce8e402c474dd22bbe498a1bf2d
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-05-11 15:14:02 +0300

    11.05.2023

--- src/test/java/day12_Synchronization_Waits/C01_WaitTest.java
+++ src/test/java/day12_Synchronization_Waits/C01_WaitTest.java
@@ -1,6 +1,11 @@
 package day12_Synchronization_Waits;
 
 import Utilities.TestBase;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.devtools.v85.webaudio.WebAudio;
 
 public class C01_WaitTest extends TestBase {
     /*
@@ -13,6 +18,27 @@ public class C01_WaitTest extends TestBase {
     4.Add buttonuna basin
     5.It’s back mesajinin gorundugunu test edin
     */
+
+    @Test
+    public void implicitWaitTesti(){
+
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+
+        driver.findElement(By.xpath("//button[@onclick='swapCheckbox()']")).click();
+
+        WebElement itsGoneYaziELementi=driver.findElement(By.xpath("//p[text()=\"It's gone!\"]"));
+
+        Assert.assertTrue(itsGoneYaziELementi.isDisplayed());
+
+        driver.findElement(By.xpath("//button[text()='Add']")).click();
+
+        WebElement itsBackYaziElementi=driver.findElement(By.xpath("//p[text()=\"It's back!\"]"));
+
+        Assert.assertTrue(itsBackYaziElementi.isDisplayed());
+
+
+
+    }
 
 
 }
