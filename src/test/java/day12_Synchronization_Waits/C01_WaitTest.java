b4ad64f06ee9a50239d0bf7cd51ba6a1ea016dfe
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-05-11 15:37:20 +0300

    11.05.2023

--- src/test/java/day12_Synchronization_Waits/C01_WaitTest.java
+++ src/test/java/day12_Synchronization_Waits/C01_WaitTest.java
@@ -6,6 +6,10 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.devtools.v85.webaudio.WebAudio;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class C01_WaitTest extends TestBase {
     /*
@@ -35,10 +39,34 @@ public class C01_WaitTest extends TestBase {
         WebElement itsBackYaziElementi=driver.findElement(By.xpath("//p[text()=\"It's back!\"]"));
 
         Assert.assertTrue(itsBackYaziElementi.isDisplayed());
-
-
-
     }
 
+    @Test
+    public void explicitlyWaitTesti(){
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+        driver.findElement(By.xpath("//button[@onclick='swapCheckbox()']")).click();
 
+        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(15));
+        /*
+        WebElement itsGoneYaziElementi=driver.findElement(By.xpath("//p[text()=\"It's gone!\"]"));
+        wait.until(ExpectedConditions.visibilityOf(itsGoneYaziElementi));
+        Assert.assertTrue(itsGoneYaziElementi.isDisplayed());
+        */
+
+        /*
+        explicitWait itsgoneWE görünür olmasını beklerken o web elementinin locatini kullanarak assert yapmak
+        sorun olur ve exeption fırlatır. Henüz görülmeyen bir Webelementin locate edilmeside otamasyon için
+        mümkün olmayabilir.
+        (HTML kodları yazan developer farklı uygulamalar yapabilir).Bu durumda bekleme işlemi için explicitWait'i locate ile birlikte kullanırız.
+        */
+
+        WebElement itsGoneYaziElementi=wait.until(ExpectedConditions.
+                visibilityOfElementLocated(By.xpath("//p[text()=\"It's gone!\"]")));
+        Assert.assertTrue(itsGoneYaziElementi.isDisplayed());
+
+        driver.findElement(By.xpath("//button[text()='Add']")).click();
+        WebElement itsBackYaziElementi=wait.until(ExpectedConditions.
+                visibilityOfElementLocated(By.xpath("//p[text()=\"It's back!\"]")));
+        Assert.assertTrue(itsBackYaziElementi.isDisplayed());
+    }
 }
