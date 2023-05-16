f97575de6ff568a94ee6db09e3b039d68c456c9a
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-05-16 15:22:10 +0300

    16.05.2023

---
+++ src/test/java/day14_Excel/C01_ReadExcel.java
@@ -0,0 +1,43 @@
+package day14_Excel;
+
+import org.apache.poi.ss.usermodel.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class C01_ReadExcel {
+
+    @Test
+    public void readExcelTesti() throws IOException {
+        // - Dosya yolunu bir String degiskene atayalim
+        String dosyaYolu="src/resources/ulkeler.xlsx";
+
+        // - FileInputStream objesi olusturup,parametre olarak dosya yolunu girelim
+        FileInputStream fis=new FileInputStream(dosyaYolu);
+
+        // - Workbook objesi olusturalim,parameter olarak fileInputStream objesini girelim
+        // - WorkbookFactory.create(fileInputStream)
+        Workbook workbook= WorkbookFactory.create(fis); // ==> Workbook objesiyle fis objesi ile akışa aldğımız bir excell dosyası create ettik.
+
+        // - Sheet objesi olusturun workbook.getSheetAt(index)
+        Sheet sheet=workbook.getSheet("Sayfa1"); // ==> Excel dosyamızda çalışmak istediğimiz sayfayı bu şekilde seçelim.
+
+        // - Row objesi olusturun sheet.getRow(index)
+        Row row=sheet.getRow(3); // ==> Sayfa birdeki ücüncü satırı bu şekilde seçeriz.
+
+        // - Cell objesi olusturun row.getCell(index)
+        Cell cell=row.getCell(3); // ==> Satır seçimi yapıldıktan sonra hücre seçimi bu şekilde yapılır.
+
+        System.out.println(cell);
+
+        //3.index'deki satırın  3. indexindeki datanın Cezayir olduğunu test edin.
+        String expectedData="Cezayir";
+        String actualData=cell.toString();
+
+        Assert.assertEquals(expectedData,actualData);
+
+    }
+}
