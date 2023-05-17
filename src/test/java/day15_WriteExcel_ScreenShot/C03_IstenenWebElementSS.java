5bde0044db4b77dce208437d7e2381ad6184450c
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-05-17 11:04:49 +0300

    17.05.2023

---
+++ src/test/java/day15_WriteExcel_ScreenShot/C03_IstenenWebElementSS.java
@@ -0,0 +1,13 @@
+package day15_WriteExcel_ScreenShot;
+
+import Utilities.TestBase;
+import org.junit.Test;
+
+public class C03_IstenenWebElementSS extends TestBase {
+    @Test
+    public void webElementSS() {
+
+        // amazona gidip nutella aratalim
+        // ve sonra
+    }
+}
