1c4cf784ac52c6285606c8c8fdf1d3adc664995e
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-05-20 12:16:52 +0300

    20.05.2023

--- src/test/java/day15_WriteExcel_ScreenShot/C03_IstenenWebElementSS.java
+++ src/test/java/day15_WriteExcel_ScreenShot/C03_IstenenWebElementSS.java
@@ -1,13 +1,32 @@
 package day15_WriteExcel_ScreenShot;
 
 import Utilities.TestBase;
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebElement;
+
+import java.io.File;
+import java.io.IOException;
 
 public class C03_IstenenWebElementSS extends TestBase {
     @Test
-    public void webElementSS() {
+    public void webElementSS() throws IOException {
 
         // amazona gidip nutella aratalim
-        // ve sonra
+        // ve sonuc sayisinin oldugu web elementin fotosunu cekelim
+
+        driver.get("https://www.amazon.com");
+        WebElement aramaKutusu=driver.findElement(By.id("twotabsearchtextbox"));
+        aramaKutusu.sendKeys("Nutella"+ Keys.ENTER);
+
+        WebElement sonucYaziElementi=driver.findElement(By.xpath("//div[@class='a-section a-spacing-small a-spacing-top-small']"));
+
+        File sonucYaziElementSS= new File("target/ekrangoruntuleri/sonucYaziSS.jpeg");
+        File temp=sonucYaziElementi.getScreenshotAs(OutputType.FILE);
+
+        FileUtils.copyFile(temp,sonucYaziElementSS);
     }
 }
---
+++ src/test/java/day15_WriteExcel_ScreenShot/C04_JS_Executor.java
@@ -0,0 +1,44 @@
+package day15_WriteExcel_ScreenShot;
+
+import Utilities.TestBase;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+public class C04_JS_Executor extends TestBase {
+
+    /*
+    ***Js Executors***
+
+    * Selenium ile web elementlerinin kontrollerini sağlarken selenium komutları yetersiz  kalabilir veya sorunlarla karşılaşabiliriz.
+    * Bu durumlarda laternatif olarak üstesinden gelmek için JavascriptExecutor classını dahil edebiliriz.
+    * Javascript HTML kodlara direkt erişip yönetebilen bir script dili olduğundan bize çok fazla kolaylık sağlayabilir.
+
+    Peki nasıl yapılır?
+
+    *ı.adım:            JavascriptExecutor kullanmak için öncelikle driverımızı JavascriptExecutor'a cast edelim.
+    *                           JavascriptExecutor jse=(JavascriptExecutor) driver;
+    * ıı.adım:          Kullanacağımız web elementi Locate edelim.
+    *                           WebElement sellElementi= driver.findElement(By.xpath("//a[text()='Sell']"));
+    * ııı.adım:         Js driver ilw executeScript methodunu çalıştıralım,içine parameter olarak ilgili script ve webelementi yazalım.
+    *                            jse.executeScript("ilgili script", "web element");=>örneğin Sell elementine click yapmak için==> jse.executeScript("arguments[0].click();", sellElementi);
+    *
+    * Bir de ScrollIntoView özelliği var bunu da aşağıdaki soruda görebiliriz.
+     */
+
+    @Test
+    public void JSExecutorTest() throws InterruptedException {
+        driver.get("https://www.amazon.com");
+
+        JavascriptExecutor jse=(JavascriptExecutor) driver;
+
+        WebElement signInButonu=driver.findElement(By.xpath("(//span[text()=\"Sign in\"])[2]"));
+        jse.executeScript("arguments[0].scrollIntoView(true);",signInButonu);
+
+        jse.executeScript("arguments[0].click();",signInButonu);
+
+
+        Thread.sleep(3000);
+    }
+}
